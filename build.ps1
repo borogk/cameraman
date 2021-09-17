5b15a5bc1e8b4650215514e3c09ec86f5f98ce9a
borogk <tyborogk@gmail.com>
2021-09-17 04:09:55 +0300

    Added initial implementation in bulk

---
+++ build.ps1
@@ -0,0 +1,36 @@
+# Start from clean "out" folder
+Remove-Item "$PSScriptRoot\out" -Recurse -Force
+New-Item -Path $PSScriptRoot -Name out -ItemType directory
+
+# Compile ACS scripts
+Get-ChildItem "$PSScriptRoot\src\acs" -Filter *.acs | Foreach-Object {
+	$name = $_.BaseName
+	$args = "$PSScriptRoot\src\acs\$name.acs", "$PSScriptRoot\out\$name.o"
+	& 'acc' $args
+	if (!$?)
+	{
+		exit
+	}
+}
+
+# Build "CameramanEditor.pk3"
+New-Item -Path "$PSScriptRoot\out\editor" -Name acs -ItemType directory
+Copy-Item "$PSScriptRoot\out\*.o" -Destination "$PSScriptRoot\out\editor\acs"
+Copy-Item "$PSScriptRoot\src\editor\*" -Destination "$PSScriptRoot\out\editor" -Recurse
+Compress-Archive -Path "$PSScriptRoot\out\editor\*" -DestinationPath "$PSScriptRoot\out\editor.zip" -Force
+Rename-Item -Path "$PSScriptRoot\out\editor.zip" -NewName CameramanEditor.pk3
+
+# Build "CameramanPlayer.pk3"
+New-Item -Path "$PSScriptRoot\out\player" -Name acs -ItemType directory
+Copy-Item "$PSScriptRoot\out\player.o" -Destination "$PSScriptRoot\out\player\acs"
+Copy-Item "$PSScriptRoot\out\geometry.o" -Destination "$PSScriptRoot\out\player\acs"
+Copy-Item "$PSScriptRoot\out\common.o" -Destination "$PSScriptRoot\out\player\acs"
+Copy-Item "$PSScriptRoot\src\player\*" -Destination "$PSScriptRoot\out\player"
+Compress-Archive -Path "$PSScriptRoot\out\player\*" -DestinationPath "$PSScriptRoot\out\player.zip" -Force
+Rename-Item -Path "$PSScriptRoot\out\player.zip" -NewName CameramanPlayer.pk3
+
+# Copy helper PowerShell scripts over to output
+Copy-Item "$PSScriptRoot\src\ps1\*" -Destination "$PSScriptRoot\out"
+
+# Cleanup the output folder
+Remove-Item "$PSScriptRoot\out\*" -Recurse -Exclude *.pk3, *.ps1
---
+++ src/ps1/player.ps1
@@ -0,0 +1,27 @@
+$baseArgs = '-file', "$PSScriptRoot\CameramanPlayer.pk3"
+
+if ($args[0] -eq 'load')
+{
+	$allArgs = $baseArgs + $args[1..($args.Length-1)]
+		
+	foreach ($line in Get-Content $args[1])
+	{
+		$match = $line | Select-String -Pattern '^([\w\d]+) = (.*)$'
+		$name = $match.Matches.Groups[1].Value
+		$value = $match.Matches.Groups[2].Value
+		
+		if ($name -ne $null)
+		{
+			$allArgs += '"+cman_{0} {1}"' -f $name, $value
+		}
+	}
+
+	$allArgs += '+pukename', 'Cman_PlayInPlayer'
+}
+else
+{
+	$allArgs = $baseArgs + $args
+}
+
+$gzdoom = Start-Process -FilePath 'gzdoom.exe' -ArgumentList $allArgs -PassThru
+Wait-Process -Id $gzdoom.Id
