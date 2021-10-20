8f5def50231136d5a1b06198862637bc1562d929
morb <morb@misentropic.com>
2021-10-20 09:13:25 -0400

    Forcing base10 may help.

--- src/bash/include.sh
+++ src/bash/include.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-gzdoom="gzdoom" #"/path/to/gzdoom"
+gzdoom="/home/morb/games/zdoom/gzdoom" #"/path/to/gzdoom"
 
 loadcam() {
 awk '{a=a"+cman_"$1." "$3." "}END{print a}' $1
@@ -10,7 +10,7 @@ i=$(startnum);o=0
 cat $1 | while read c 
 do 
 if [[ $c =~ END\ CAM ]]
-    then o=0;i=$(printf "%04d" $(($i+1)))
+    then o=0;i=$(printf "%04d" $(( 10#${i}+1 )))
 fi
 if [ $o -gt 0 ]
     then printf -- "$c\n" >> ${WD}/export-${i}.cman 
@@ -23,6 +23,6 @@ done
 
 startnum() {
 i=$(ls ${WD}/*.cman|sort -n|tail -1|sed "s/${WD}.*-//;s/\..*//" 2>/dev/null)
-[ -z $i ] && i=0000 || i=$(printf "%04d" $((${i}+1)))
+[ -z $i ] && i=0000 || i=$(printf "%04d" $(( 10#${i}+1 )))
 printf "$i"
 }
